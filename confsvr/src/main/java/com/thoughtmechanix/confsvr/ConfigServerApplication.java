58352ee50858325b6e644d5929d8b8c8f5606621
许帅虎 <nobunagamatu@163.com>
2020-04-23 16:09:36 +0800

    #

--- confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -11,7 +11,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigServerApplication.class, args);
+
 	}
 
 }
